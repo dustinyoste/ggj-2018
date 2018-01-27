12a6b38758ee1eb58130c3dfb5070862e42a1724
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-27 00:34:22 -0600

    on collision template for showing the UI

---
+++ Assets/Scripts/OnLockedCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Collider2D))]
+public class OnLockedCollider : MonoBehaviour
+{
+	public LockedComponent lockedObject;
+
+	protected void OnTriggerStay2D(Collider2D coll)
+	{
+		var player = coll.GetComponent<PlatformerCharacter2D>();
+		if (player == null)
+			return;
+
+		// Show the UI
+
+		// Allow interactablility
+	}
+}
