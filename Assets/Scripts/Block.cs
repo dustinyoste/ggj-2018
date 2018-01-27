769a2f6fe4f42608c208de6ab2574a88f54b10df
Zach Barnes <Zacharias3690@gmail.com>
2018-01-27 00:13:57 -0600

    Add block and lockable entity scripts

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour, ILockableEntity {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool Unlock()
+	{
+		return false;
+	}
+
+	public bool IsLocked { get; private set; }
+}
