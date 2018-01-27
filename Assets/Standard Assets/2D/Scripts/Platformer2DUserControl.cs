27f14eaa8421500c67d48c2e73635b7d163a7971
Dustin Yoste <dustin.yoste@gmail.com>
2018-01-27 01:22:04 -0600

    2 player controls

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -7,9 +7,12 @@ namespace UnityStandardAssets._2D
     [RequireComponent(typeof (PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
+        public int player;
+        public string jump;
+        public string horizontal;
+
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
-
 
         private void Awake()
         {
@@ -22,7 +25,7 @@ namespace UnityStandardAssets._2D
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_Jump = CrossPlatformInputManager.GetButtonDown(jump);
             }
         }
 
@@ -32,8 +35,8 @@ namespace UnityStandardAssets._2D
             //Debug.Log("m_jump");
             //Debug.Log(m_Jump);
             // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            bool crouch = false;//Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis(horizontal);
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
