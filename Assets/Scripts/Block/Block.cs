7151cfbb543a17d83e2077ca84d128eae8a12c72
Zach Barnes <Zacharias3690@gmail.com>
2018-01-27 00:36:16 -0600

    Add lockable entity move block into own folder

---
+++ Assets/Scripts/Block/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Block : MonoBehaviour, ILockableEntity
+{
+	public bool IsLocked;
+
+	// Use this for initialization
+	void Start ()
+	{
+		IsLocked = true;
+		var renderer = GetComponent<Renderer>();
+		renderer.material.SetColor("_Color", Color.red);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Unlock()
+	{
+		IsLocked = false;
+		
+		var renderer = GetComponent<Renderer>();
+		renderer.material.SetColor("_Color", Color.green);
+
+	}
+}
--- Assets/Scripts/ILockableEntity.cs
+++ Assets/Scripts/ILockableEntity.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 
 public interface ILockableEntity
 {
-    bool Unlock();
-    bool IsLocked { get; }
+    void Unlock();
 }
