f59d374b1255adf45e000a901c8f9e6977d7195c
Zach Barnes <Zacharias3690@gmail.com>
2018-01-27 16:53:29 -0600

    Add ButtonCollider interaction

---
+++ Assets/Scripts/OnButtonCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnButtonCollider : MonoBehaviour
+{
+    public GameObject UnlockComponent;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       UnlockComponent.GetComponent<IOnUnlock>().OnUnlock();
+    }
+}
