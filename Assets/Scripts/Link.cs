1011da22813733928a873a387a2bcb4db09b24b4
Dustin Yoste <dustin.yoste@gmail.com>
2018-01-27 02:13:30 -0600

    connected blocks

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour {
+
+    private Transform m_Link1;
+    private Transform m_Link2;
+    private LineRenderer m_LineRenderer;
+
+
+    private void Awake()
+    {
+        m_Link1 = transform.GetChild(0);
+        m_Link2 = transform.GetChild(1);
+        m_LineRenderer = GetComponent<LineRenderer>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_LineRenderer.SetPosition(0, m_Link1.position);
+		m_LineRenderer.SetPosition(1, m_Link2.position);
+	}
+}
