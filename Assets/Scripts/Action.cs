dcf3a863aea3454420b038d501cc644eb1d9ae24
Zach Barnes <Zacharias3690@gmail.com>
2018-01-27 04:00:20 -0600

    Create Dot and Line Prefabs

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Action : MonoBehaviour, IAction
+{
+    public float MinKeyDownTime;
+    public float MaxKeyDownTime;
+    public bool HasRun { get; private set; }
+    public bool IsSuccess { get; private set; }
+
+    private bool _isRunning;
+    private Color _defaultColor;
+    
+    protected float ActionTime;
+    protected Transform ProgressTransform;
+
+    void Start()
+    {
+        HasRun = false;
+        IsSuccess = false;
+        _isRunning = false;
+        ActionTime = 0;
+        _defaultColor = GetComponent<Renderer>().sharedMaterial.GetColor("_Color");
+        ProgressTransform = transform.Find("ProgressContainer").gameObject.transform;
+    }
+
+    void Update()
+    {
+        if (_isRunning)
+        {
+            ActionTime += Time.deltaTime;
+            CheckAction();
+            ShowProgress();
+        }
+    }
+    
+    public void StartAction()
+    {
+        HasRun = false;
+        IsSuccess = false;
+        _isRunning = true;
+        ActionTime = 0;
+    }
+
+    public void CheckAction()
+    {
+        if (ActionTime > MaxKeyDownTime)
+        {
+            EndAction(false);
+        }
+    }
+
+    public void EndAction(bool? result = null)
+    {
+        HasRun = true;
+        _isRunning = false;
+
+        if (result != null)
+        {
+            IsSuccess = (bool) result;
+        }
+        else if (ActionTime < MinKeyDownTime)
+        {
+            IsSuccess = false;
+        }
+        else if(ActionTime > MinKeyDownTime)
+        {
+            IsSuccess = true;
+        }
+
+        if (IsSuccess) 
+            HandleSuccess();
+        else 
+            HandleFailure();
+        
+        ActionTime = 0;
+    }
+
+    public void Reset()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.SetColor("_Color", _defaultColor);
+        Start();
+    }
+
+    void HandleSuccess()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.SetColor("_Color", Color.green);
+    }
+
+    void HandleFailure()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.SetColor("_Color", Color.red);
+    }
+
+    protected virtual void ShowProgress()
+    {
+       
+    }
+}
--- Assets/Scripts/DotAction.cs
+++ Assets/Scripts/DotAction.cs
@@ -3,93 +3,17 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class DotAction : MonoBehaviour, IAction
+public class DotAction : Action
 {
-    public float MinKeyDownTime;
-    public float MaxKeyDownTime;
-    public bool HasRun { get; private set; }
-    public bool IsSuccess { get; private set; }
-
-    private bool _isRunning;
-    private float _actionTime;
-    private Color _defaultColor;
-
-    void Start()
+    private bool _isValidTime = false;
+    protected override void ShowProgress()
     {
-        HasRun = false;
-        IsSuccess = false;
-        _isRunning = false;
-        _actionTime = 0;
-        _defaultColor = GetComponent<Renderer>().material.GetColor("_Color");
-    }
-
-    void Update()
-    {
-        if (_isRunning)
-        {
-            _actionTime += Time.deltaTime;
-            CheckAction();
-        }
-    }
-    
-    public void StartAction()
-    {
-        HasRun = false;
-        IsSuccess = false;
-        _isRunning = true;
-        _actionTime = 0;
-    }
-
-    public void CheckAction()
-    {
-        if (_actionTime > MaxKeyDownTime)
-        {
-            EndAction(false);
-        }
-    }
-
-    public void EndAction(bool? result = null)
-    {
-        HasRun = true;
-        _isRunning = false;
-
-        if (result != null)
-        {
-            IsSuccess = (bool) result;
-        }
-        else if (_actionTime < MinKeyDownTime)
-        {
-            IsSuccess = false;
-        }
-        else if(_actionTime > MinKeyDownTime)
-        {
-            IsSuccess = true;
-        }
-
-        if (IsSuccess) 
-            HandleSuccess();
-        else 
-            HandleFailure();
+        ProgressTransform.localScale = new Vector2((ActionTime / MaxKeyDownTime), (ActionTime / MaxKeyDownTime));
         
-        _actionTime = 0;
-    }
-
-    public void Reset()
-    {
-        var renderer = GetComponent<Renderer>();
-        renderer.material.SetColor("_Color", _defaultColor);
-        Start();
-    }
-
-    void HandleSuccess()
-    {
-        var renderer = GetComponent<Renderer>();
-        renderer.material.SetColor("_Color", Color.green);
-    }
-
-    void HandleFailure()
-    {
-        var renderer = GetComponent<Renderer>();
-        renderer.material.SetColor("_Color", Color.red);
+        if (!_isValidTime && ActionTime > MinKeyDownTime)
+        {
+            _isValidTime = true;
+            ProgressTransform.Find("Progress").GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        }
     }
 }
