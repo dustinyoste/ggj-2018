fd2ce0cf075918c2e4581706d0503d5ff4734174
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-28 12:51:11 -0600

    Beat the game event

---
+++ Assets/Scripts/EndLevelEVent.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class EndLevelEVent : MonoBehaviour
+{
+	public GameObject level;
+	public GameObject playerUI;
+	public GameObject winUI;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			level.SetActive(false);
+			playerUI.SetActive(false);
+			winUI.SetActive(true);
+		}
+	}
+}
