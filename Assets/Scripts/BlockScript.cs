59585e845708ecb34b90703cac0d3756a5e5afb4
Dustin Yoste <dustin.yoste@gmail.com>
2018-01-27 19:57:24 -0600

    move blocks together

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class BlockScript : MonoBehaviour, IOnUnlock {
     public GameObject Buddy;
 
-    private bool BeingMoved;
+    private bool isBeingMovedByPlayer;
+    private bool isBeingMovedByBuddy;
 	public bool IsLocked = true;
     
     private Vector2 velocity;
 
+    public void BeingMovedByBuddy(bool isBeingMoved) {
+        isBeingMovedByBuddy = isBeingMoved;
+    }
+
+    public void BeingMovedByPlayer(bool isBeingMoved) {
+        isBeingMovedByPlayer = isBeingMoved;
+    }
+
     private void FixedUpdate()
     {
+        if (isBeingMovedByBuddy || isBeingMovedByPlayer) {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        } else {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+        }
+
         if (Buddy != null)
         {
-            if (velocity.magnitude > 0.00 && !BeingMoved)
+            var velocity = GetComponent<Rigidbody2D>().velocity; 
+            if (velocity.magnitude > 0.00 && !isBeingMovedByBuddy)
             {
                 Buddy.GetComponent<Rigidbody2D>().velocity = velocity;
-                Buddy.GetComponent<BlockScript>().BeingMoved = true;
+                Buddy.GetComponent<BlockScript>().BeingMovedByBuddy(true);
             } else {
-                Buddy.GetComponent<BlockScript>().BeingMoved = false;
+                Buddy.GetComponent<BlockScript>().BeingMovedByBuddy(false);
             }     
         }
     }
