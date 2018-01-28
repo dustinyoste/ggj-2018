763e459ee43ff36e057414d3e04012fcfb4ea32e
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-27 22:42:59 -0600

    Adding the game controller skeleton so we can start tracking our progress through the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	#region Singleton
+
+	public static GameController Instance
+	{
+		get
+		{
+			return sInstance;
+		}
+	}
+
+	public static bool TryGetInstance(out GameController returnVal)
+	{
+		returnVal = sInstance;
+		if (returnVal == null) {
+			Debug.LogWarning(string.Format("Couldn't access {0}", typeof(GameController).Name));
+		}
+		return (returnVal != null);
+	}
+
+	private static GameController sInstance;
+
+	#endregion
+
+	#region Monobehaviours
+
+	protected void Awake()
+	{
+		if (sInstance == null) {
+			sInstance = this;
+		}
+	}
+
+	protected void OnDestroy()
+	{
+		sInstance = null;
+	}
+
+	#endregion
+}
