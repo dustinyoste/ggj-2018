ec93e1c41623541eb8efab8ba1a69c93f85944e3
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-28 02:05:47 -0600

    Editor script to unlock all blocks on the current level

---
+++ Assets/Editor/HelperEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class HelperEditor
+{
+	[MenuItem("Cheat/UnlockAll", false, 21)]
+	static void UnlockAllLockedComponents()
+	{
+		LockedComponent[] objs = Resources.FindObjectsOfTypeAll<LockedComponent>();
+		foreach (LockedComponent obj in objs) {
+			obj.Unlock();
+		}
+	}
+}
