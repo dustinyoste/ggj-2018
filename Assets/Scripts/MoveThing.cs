c601eef499d21ba295594e1c63aa168c5d3e442b
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-28 09:49:10 -0600

    third puzzle is in the books

--- Assets/Scripts/MoveThing.cs
+++ Assets/Scripts/MoveThing.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveThing : MonoBehaviour, IOnUnlock {
+public class MoveThing : MonoBehaviour, IOnUnlock
+{
+	public float distance = -1.31f;
+	public bool force;
+	void Start()
+	{
+	}
 
-    void Start() {
-    }
+	private void Update()
+	{
+		if(force){
+			force = false;
+			OnUnlock();
+		}
+	}
 
-    public void OnUnlock() {
-        transform.position = new Vector2(transform.position.x + 10, transform.position.y);
-    }
+	public void OnUnlock()
+	{
+		transform.position = new Vector2(transform.position.x, transform.position.y + distance);
+	}
 }
