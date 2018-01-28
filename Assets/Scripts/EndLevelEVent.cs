a9e8b978bb7462d662cec2c14eb95c291d2e34a4
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-28 13:11:46 -0600

    Added a win condition

--- Assets/Scripts/EndLevelEVent.cs
+++ Assets/Scripts/EndLevelEVent.cs
@@ -3,16 +3,39 @@ using UnityEngine;
 
 public class EndLevelEVent : MonoBehaviour
 {
-	public GameObject level;
+	public GameObject blocks;
+	public GameObject levers;
+	public GameObject players;
+	public GameObject ground;
 	public GameObject playerUI;
 	public GameObject winUI;
+	private bool didWin;
+
+	protected void Update()
+	{
+		if (didWin) {
+			if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Escape)) {
+#if UNITY_EDITOR
+				// Application.Quit() does not work in the editor so
+				// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+				UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+			}
+		}
+	}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player") {
-			level.SetActive(false);
+			blocks.SetActive(false);
+			levers.SetActive(false);
+			players.SetActive(false);
+			ground.SetActive(false);
 			playerUI.SetActive(false);
 			winUI.SetActive(true);
+			didWin = true;
 		}
 	}
 }
