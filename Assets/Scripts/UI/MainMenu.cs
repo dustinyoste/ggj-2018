12a4f2e0a33ac91adb49973720a6ffd5e9ecb4af
Kamau Vassall <kamau.vassall@gmail.com>
2018-01-28 01:13:13 -0600

    Got the main menu working with the video player and backup image

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,27 +1,66 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 [DisallowMultipleComponent]
 public class MainMenu : MonoBehaviour
 {
-
-	public Button startButton;
+	public VideoPlayer videoplayer;
+	public Image background;
 	public string sceneName = "main_scene";
 
 	// Use this for initialization
 	void Start()
 	{
-		startButton.onClick.AddListener(PlayGame);
+		background.enabled = true;
+		videoplayer.started += (VideoPlayer source) =>
+		{
+			background.enabled = !source.isPlaying;
+		};
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		var hitEnterKey = Input.GetKey(KeyCode.KeypadEnter)
+			|| Input.GetKey(KeyCode.Return)
+							   || Input.GetKey((KeyCode.Space));
 
+		var hitLetter = false;
+		foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode))) {
+			if (kcode >= KeyCode.A && kcode <= KeyCode.Z && Input.GetKeyDown(kcode)) {
+				Debug.Log("KeyCode down: " + kcode);
+				hitLetter = true;
+			}
+		}
+
+		if (hitEnterKey || hitLetter) {
+			PlayGame();
+		} else if (Input.GetKey(KeyCode.Escape)) {
+			ExitGame();
+		}
+	}
+
+	protected void OnMouseDown()
+	{
+		PlayGame();
 	}
 
 	private void PlayGame()
 	{
 		UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+	}
+
+	private void ExitGame()
+	{
+#if UNITY_EDITOR
+		// Application.Quit() does not work in the editor so
+		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
 	}
 }
